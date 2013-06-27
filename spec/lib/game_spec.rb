60ec4a0ae7dd6a32f0e83db3f5e7fe8ec99974cd
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-26 21:45:16 -0600

    Fail: continue rolling

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -13,10 +13,14 @@ describe Game do
   A_NUMBER_OF_PINS = 3
   ANOTHER_NUMBER_OF_PINS = 1
   describe 'roll' do
+    let(:game) { described_class.new([]) }
     it 'adds the result of the roll' do
-      game = described_class.new([])
       game_after_roll = game.roll(A_NUMBER_OF_PINS)
       expect(game_after_roll.rolls).to eq [A_NUMBER_OF_PINS]
+    end
+    it 'plays other roll' do
+      game_after_second_roll = game.roll(A_NUMBER_OF_PINS).roll(ANOTHER_NUMBER_OF_PINS)
+      expect(game_after_second_roll.rolls).to eq [A_NUMBER_OF_PINS, ANOTHER_NUMBER_OF_PINS]
     end
   end
 end
