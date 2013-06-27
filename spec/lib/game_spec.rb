1151cd838bedf41a69b4d109636a26275053a07c
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-26 22:05:31 -0600

    Fail: calculates the score of a gutter game

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -12,15 +12,26 @@ end
 describe Game do
   A_NUMBER_OF_PINS = 3
   ANOTHER_NUMBER_OF_PINS = 1
+  GUTTER_ROLL = 0
+
   describe 'roll' do
     let(:game) { described_class.new([]) }
+
     it 'adds the result of the roll' do
       game_after_roll = game.roll(A_NUMBER_OF_PINS)
       expect(game_after_roll.rolls).to eq [A_NUMBER_OF_PINS]
     end
+
     it 'plays other roll' do
       game_after_second_roll = game.roll(A_NUMBER_OF_PINS).roll(ANOTHER_NUMBER_OF_PINS)
       expect(game_after_second_roll.rolls).to eq [A_NUMBER_OF_PINS, ANOTHER_NUMBER_OF_PINS]
+    end
+  end
+
+  describe 'score' do
+    it 'calulates the score for a gutter game' do
+      game = described_class.new([GUTTER_ROLL] * 20)
+      expect(game.score).to eq 0
     end
   end
 end
