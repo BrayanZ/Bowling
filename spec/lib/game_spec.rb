c91716c214b0e2608fcb0c46c28dfdee10e8c2b4
Brayan Zúñiga <brayan@pernix-solutions.com>
2013-06-26 21:44:12 -0600

    Pass

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,5 +1,17 @@
+class Game
+  attr_reader :rolls
+  def initialize(rolls)
+    @rolls = rolls
+  end
+
+  def roll(pins)
+    Game.new [pins]
+  end
+end
+
 describe Game do
   A_NUMBER_OF_PINS = 3
+  ANOTHER_NUMBER_OF_PINS = 1
   describe 'roll' do
     it 'adds the result of the roll' do
       game = described_class.new([])
